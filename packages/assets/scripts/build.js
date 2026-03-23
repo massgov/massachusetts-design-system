cab4ffda969354648ee03cadcf8628a418f185a3
Minghua Sun <clairesunstudio@gmail.com>
2026-03-23 17:29:49 -0400

    move build steps and package meta into the assets package root

---
+++ packages/assets/scripts/build.js
@@ -0,0 +1,36 @@
+const fs = require('fs/promises');
+const path = require('path');
+const prepIcons = require('../icons/scripts/prepIcons');
+
+const packageRoot = path.resolve(__dirname, '..');
+const distDir = path.join(packageRoot, 'dist');
+
+async function cleanDist() {
+  await fs.rm(distDir, { recursive: true, force: true });
+}
+
+async function ensureDir(dirPath) {
+  await fs.mkdir(dirPath, { recursive: true });
+}
+
+async function copyDir(from, to) {
+  await ensureDir(path.dirname(to));
+  await fs.cp(from, to, { recursive: true });
+}
+
+async function build() {
+  await cleanDist();
+
+  prepIcons(path.join(packageRoot, 'icons/static'));
+
+  await Promise.all([
+    copyDir(path.join(packageRoot, 'icons/static'), path.join(distDir, 'icons')),
+    copyDir(path.join(packageRoot, 'animation'), path.join(distDir, 'animation')),
+    copyDir(path.join(packageRoot, 'state-seal'), path.join(distDir, 'state-seal'))
+  ]);
+}
+
+build().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
