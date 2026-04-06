11354f2cdb7daa767fff5e7bd7833efc7af6e846
S. Kimball <sarah.kimball@mass.gov>
2026-04-06 13:10:29 -0400

    fix conflicts and rebase

--- packages/components/state-banner/toggle.js
+++ packages/components/state-banner/toggle.js
@@ -1,7 +1,7 @@
 //Accessible code from Alexa on the Access team
 document.addEventListener('DOMContentLoaded', function() {
-    const trigger = document.querySelector('.ma-state-banner__container');
-    const triggerChevron = document.querySelector('.ma-state-banner__caret');
+    const trigger = document.querySelector('.mds-state-banner__container');
+    const triggerChevron = document.querySelector('.mds-state-banner__caret');
 
     trigger.addEventListener('click', function() {
         const content = this.nextElementSibling; // Get the next sibling (the content div)
@@ -11,13 +11,13 @@ document.addEventListener('DOMContentLoaded', function() {
         
         content.style.display = "none";
         trigger.setAttribute("aria-expanded", "false");
-        triggerChevron.classList.remove("ma-state-banner__caret--expanded");
+        triggerChevron.classList.remove("mds-state-banner__caret--expanded");
         } else {
         // Content is currently collapsed, so expand it
 
         content.style.display = "flex"; // show the hidden content
         trigger.setAttribute("aria-expanded", "true");
-        triggerChevron.classList.add("ma-state-banner__caret--expanded");
+        triggerChevron.classList.add("mds-state-banner__caret--expanded");
         } 
     });
 });
