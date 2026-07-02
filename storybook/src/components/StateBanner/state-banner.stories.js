0335008609a2d20daf36c1f8385b094c61af8f08
Minghua Sun <clairesunstudio@gmail.com>
2026-07-02 13:28:54 -0400

    remove state banner controls

--- storybook/src/components/StateBanner/state-banner.stories.js
+++ storybook/src/components/StateBanner/state-banner.stories.js
@@ -2,10 +2,6 @@ import '@massds/mds-components/state-banner.css';
 import {
   stateBannerDefaults
 } from '../../../../packages/components/src/state-banner/state-banner.data.js';
-import {
-  stateSealFileTypes,
-  stateSealVariants
-} from '../../../../packages/components/src/state-seal/state-seal.data.js';
 import { renderStateBanner } from '../../utils/component-renderers.js';
 
 // Storybook render functions return an HTML element.
@@ -25,39 +21,9 @@ function renderPlayground(args) {
   return createPreview(renderStateBanner(args));
 }
 
-const stateBannerControls = {
-  panelId: {
-    control: 'text',
-    description: 'The panel id linked from the details element.'
-  },
-  sealVariant: {
-    control: 'select',
-    description: 'State seal variant to render.',
-    options: stateSealVariants
-  },
-  sealFileType: {
-    control: 'select',
-    description: 'State seal asset file type.',
-    options: stateSealFileTypes
-  },
-  summaryLabel: {
-    control: 'text',
-    description: 'Accessible label for the banner section.'
-  }
-};
-
-const defaultPlaygroundArgs = {
-  panelId: stateBannerDefaults.panelId,
-  sealVariant: stateBannerDefaults.sealVariant,
-  sealFileType: stateBannerDefaults.sealFileType,
-  summaryLabel: stateBannerDefaults.summaryLabel
-};
-
 const meta = {
   title: 'Components/State Banner',
-  render: renderPlayground,
-  argTypes: stateBannerControls,
-  args: defaultPlaygroundArgs
+  render: renderPlayground
 };
 
 export default meta;
