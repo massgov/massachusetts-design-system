934da59f1cb824638e5d0a82379031bd2b7930c1
Minghua Sun <clairesunstudio@gmail.com>
2026-07-02 13:19:12 -0400

    update seal story and add variants

--- storybook/src/foundations/StateSeal/state-seal.stories.js
+++ storybook/src/foundations/StateSeal/state-seal.stories.js
@@ -1,7 +1,8 @@
-import { A } from 'storybook/internal/components';
+import './state-seal.examples.css';
 import {
+  stateSealDefaults,
   stateSealFileTypes,
-  stateSealVariants 
+  stateSealVariants
 } from '../../../../packages/components/src/state-seal/state-seal.data.js';
 import { renderStateSeal } from '../../utils/component-renderers.js';
 
@@ -19,12 +20,12 @@ function createPreview(html, className = '') {
 }
 
 const stateSealControls = {
-  sealVariant: {
+  variant: {
     control: 'select',
     description: 'State seal variant to render.',
     options: stateSealVariants
   },
-  sealFileType: {
+  fileType: {
     control: 'select',
     description: 'State seal asset file type.',
     options: stateSealFileTypes
@@ -32,15 +33,67 @@ const stateSealControls = {
 };
 
 function renderPlayground(args) {
-  return createPreview(renderStateSeal(args));
+  return createPreview(renderStateSeal(args), 'mds-state-seal-playground');
 }
+
+function renderVariantCard(variant, fileType) {
+  const previewModifierClass = variant === 'white'
+    ? ' mds-state-seal-gallery__preview--dark'
+    : '';
+
+  return `
+    <div class="mds-state-seal-gallery__item">
+      <div class="mds-state-seal-gallery__preview${previewModifierClass}">
+        ${renderStateSeal({
+          ...stateSealDefaults,
+          fileType,
+          variant
+        })}
+      </div>
+      <code class="mds-state-seal-gallery__label">${variant}</code>
+    </div>
+  `;
+}
+
+function renderAllVariants(args = {}) {
+  const requestedFileType = args.fileType;
+  const fileType = stateSealFileTypes.includes(requestedFileType)
+    ? requestedFileType
+    : stateSealDefaults.fileType;
+  let galleryHtml = '';
+
+  for (const variant of stateSealVariants) {
+    galleryHtml += renderVariantCard(variant, fileType);
+  }
+
+  return createPreview(galleryHtml, 'mds-state-seal-gallery');
+}
+
+const defaultPlaygroundArgs = {
+  fileType: stateSealDefaults.fileType,
+  variant: stateSealDefaults.variant
+};
 
 const meta = {
   title: 'Foundations/State Seal',
   render: renderPlayground,
-  argTypes: stateSealControls
+  argTypes: stateSealControls,
+  args: defaultPlaygroundArgs
 };
 
 export default meta;
 
 export const Playground = {};
+
+export const AllVariants = {
+  render: renderAllVariants,
+  args: {
+    fileType: stateSealDefaults.fileType
+  },
+  parameters: {
+    controls: {
+      include: ['fileType']
+    },
+    layout: 'padded'
+  }
+};
