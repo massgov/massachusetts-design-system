0424bb47bf6bdac0d02ee54ae8bbdf94914f0b90
Minghua Sun <clairesunstudio@gmail.com>
2026-07-30 14:00:37 -0400

    add required fields

--- packages/components/src/footer/footer.schema.js
+++ packages/components/src/footer/footer.schema.js
@@ -5,27 +5,33 @@ const iconOptions = ['', ...iconNames];
 export const footerSchema = {
   theme: {
     type: 'enum',
+    required: false,
     default: 'Neutral',
     options: ['Neutral', 'Primary']
   },
   siteNameId: {
     type: 'string',
+    required: false,
     default: 'mds-footer-site-name'
   },
   siteName: {
     type: 'string',
+    required: true,
     default: 'Site Name'
   },
   sealAlt: {
     type: 'string',
+    required: false,
     default: ''
   },
   socialLabel: {
     type: 'string',
+    required: false,
     default: 'Social media links'
   },
   socialLinks: {
     type: 'array',
+    required: false,
     items: {
       type: 'object',
       fields: {
@@ -46,59 +52,73 @@ export const footerSchema = {
     }
   },
   descriptionHtml: {
-    type: 'string'
+    type: 'string',
+    required: false
   },
   contactHeadingId: {
     type: 'string',
+    required: false,
     default: 'mds-footer-contact-heading'
   },
   contactHeading: {
     type: 'string',
+    required: false,
     default: 'Contact'
   },
   contactItems: {
     type: 'array',
+    required: false,
     items: {
       type: 'object',
       fields: {
         icon: {
           type: 'icon',
+          required: false,
           options: iconOptions
         },
         addressLines: {
           type: 'array',
+          required: false,
           items: {
             type: 'string'
           }
         },
         note: {
-          type: 'string'
+          type: 'string',
+          required: false
         },
         href: {
-          type: 'string'
+          type: 'string',
+          required: false
         },
         text: {
-          type: 'string'
+          type: 'string',
+          required: false
         }
       }
     }
   },
   linkGroups: {
     type: 'array',
+    required: false,
     items: {
       type: 'object',
       fields: {
         headingId: {
-          type: 'string'
+          type: 'string',
+          required: false
         },
         heading: {
-          type: 'string'
+          type: 'string',
+          required: false
         },
         eyebrow: {
-          type: 'boolean'
+          type: 'boolean',
+          required: false
         },
         links: {
           type: 'array',
+          required: false,
           items: {
             type: 'object',
             fields: {
@@ -118,10 +138,12 @@ export const footerSchema = {
   },
   legalLabel: {
     type: 'string',
+    required: false,
     default: 'Required policies'
   },
   legalLinks: {
     type: 'array',
+    required: false,
     items: {
       type: 'object',
       fields: {
@@ -137,13 +159,16 @@ export const footerSchema = {
     }
   },
   fundingText: {
-    type: 'string'
+    type: 'string',
+    required: false
   },
   trademarkHtml: {
-    type: 'string'
+    type: 'string',
+    required: false
   },
   itemIcon: {
     type: 'icon',
+    required: false,
     default: '',
     options: iconOptions
   }
