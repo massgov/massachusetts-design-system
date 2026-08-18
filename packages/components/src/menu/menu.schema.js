854d9b0383df49916bd3b21b85a9bf4a4598691c
S. Kimball <sarah.kimball@mass.gov>
2026-08-18 15:26:25 -0400

    fixed conflict

---
+++ packages/components/src/menu/menu.schema.js
@@ -0,0 +1,55 @@
+export const menuSchema = {
+  id: {
+    type: 'string',
+    default: 'mds-menu'
+  },
+  className: {
+    type: 'string',
+    default: 'mds-popup-menu'
+  },
+  ariaLabel: {
+    type: 'string',
+    default: 'Menu'
+  },
+  ariaLabelledby: {
+    type: 'string',
+    default: ''
+  },
+  noIcons: {
+    type: 'boolean',
+    default: false
+  },
+  items: {
+    type: 'array',
+    default: [
+      {
+        type: 'button',
+        buttonType: 'button',
+        label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non diam sit amet lacus molestie dapibus molestie eu diam. Donec vitae diam in nunc ultricies euismod.',
+        iconName: 'smiley',
+        iconWeight: 'Bold'
+      },
+      {
+        type: 'button',
+        buttonType: 'button',
+        label: 'Select item',
+        iconName: 'smiley',
+        iconWeight: 'Bold'
+      },
+      {
+        type: 'button',
+        buttonType: 'button',
+        label: 'Select item',
+        iconName: 'smiley',
+        iconWeight: 'Bold'
+      },
+      {
+        type: 'button',
+        buttonType: 'button',
+        label: 'Select item',
+        iconName: 'smiley',
+        iconWeight: 'Bold'
+      }
+    ]
+  }
+};
