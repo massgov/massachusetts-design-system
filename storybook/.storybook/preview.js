d9c7db3ffd0dfad7a3280ec1b3fbf03c8286485b
Shea VanLaningham <sheavanlaningham@Sheas-macbook-air.local>
2026-09-23 11:04:50 -0500

    Type styles documentation page initial commit

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -13,7 +13,7 @@ const preview = {
           'Overview',
           ['Introduction', 'Getting Started'],
           'Foundations',
-          ['Design Tokens', 'Color Tokens', ['Overview', 'Guidance', 'Code', 'Playground']],
+          ['Design Tokens', 'Color Tokens', 'Type Styles', ['Overview', 'Guidance', 'Code', 'Playground']],
           'Components',
           ['*', ['Overview', 'Guidance', 'Code', 'Playground']]
         ],
