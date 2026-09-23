b88c5c43f48307723cf41284edd3e980a099a1ae
Minghua Sun <clairesunstudio@gmail.com>
2026-09-23 12:09:40 -0400

    render footer template in preview and custom data

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -4,60 +4,71 @@ import '@massds/mds-components/action-link.css';
 import '@massds/mds-components/footer.css';
 import '@massds/mds-components/icon-button.css';
 import { DocsContainer } from '@storybook/addon-docs/blocks';
-import { createElement, Fragment, useEffect, useState } from 'react';
+import { createElement, Fragment } from 'react';
 import storybookPackage from '../package.json';
+import { renderFooter } from '../src/utils/component-renderers';
 import './docs-markdown.css';
 import './preview.css';
 import { massdsDocsTheme } from './theme';
 
-const footerHtmlUrl = new URL(
-  `${import.meta.env.BASE_URL}components/footer/footer.storybook.html`,
-  window.location.origin
-).href;
 const stateAssetsVersion = storybookPackage.dependencies['@massds/mds-assets'];
 const footerSealSrc = `https://unpkg.com/@massds/mds-assets@${stateAssetsVersion}/dist/state-seal/state-seal-black.png`;
-let footerMarkupPromise;
 
-function loadFooterMarkup() {
-  if (!footerMarkupPromise) {
-    footerMarkupPromise = fetch(footerHtmlUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Unable to load the Storybook footer: ${response.status}`);
-        }
+const sharedFooterData = {
+  theme: 'Neutral',
+  siteNameId: 'storybook-footer-site-name',
+  siteName: 'Massachusetts Design System',
+  sealAlt: '',
+  socialLinks: [],
+  descriptionHtml: 'Storybook is the implementation reference for the Massachusetts Design System. It provides reusable components, design tokens, accessibility guidance, and code examples for Commonwealth digital products.',
+  contactHeadingId: 'storybook-footer-contact-heading',
+  contactHeading: 'Contact',
+  contactItems: [
+    {
+      icon: 'star',
+      href: 'mailto:designsystem@mass.gov',
+      text: 'Share feedback'
+    },
+    {
+      icon: 'globe',
+      href: 'https://mass.gov/designsystem',
+      text: 'mass.gov/designsystem'
+    },
+    {
+      icon: 'envelope',
+      href: 'mailto:designsystem@mass.gov',
+      text: 'designsystem@mass.gov'
+    }
+  ],
+  linkGroups: [],
+  legalLabel: 'Footer links',
+  legalLinks: [
+    {
+      href: 'https://www.mass.gov/info-details/commonwealth-of-massachusetts-executive-department-digital-accessibility-statement',
+      text: 'Digital Accessibility Statement'
+    },
+    {
+      href: 'https://www.mass.gov/policy-advisory/massgov-privacy-policy',
+      text: 'Privacy Notice'
+    }
+  ],
+  showOptionalLegalLinks: false,
+  legalLinksOptional: [],
+  fundingText: '',
+  trademarkHtml: '<strong>&copy; 2026 Commonwealth of Massachusetts.</strong><br />Mass.gov&reg; is a registered service mark of the Commonwealth of Massachusetts.'
+};
 
-        return response.text();
-      })
-      .then((markup) => {
-        const template = document.createElement('template');
+function renderSharedFooter() {
+  const template = document.createElement('template');
 
-        template.innerHTML = markup;
-        template.content.querySelector('.mds-footer__seal')?.setAttribute('src', footerSealSrc);
-        return template.innerHTML;
-      });
-  }
+  template.innerHTML = renderFooter(sharedFooterData);
+  template.content.querySelector('.mds-footer__seal')?.setAttribute('src', footerSealSrc);
 
-  return footerMarkupPromise;
+  return template.innerHTML;
 }
 
 function StorybookDocsContainer({ children, ...props }) {
-  const [footerMarkup, setFooterMarkup] = useState('');
-
-  useEffect(() => {
-    let isMounted = true;
-
-    loadFooterMarkup()
-      .then((markup) => {
-        if (isMounted) {
-          setFooterMarkup(markup);
-        }
-      })
-      .catch((error) => console.error(error));
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);
+  const footerMarkup = renderSharedFooter();
 
   return createElement(
     Fragment,
