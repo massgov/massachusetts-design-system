bb52a8c163b9534f4fdb06fcc40ded4c51c9a7bb
Minghua Sun <clairesunstudio@gmail.com>
2026-09-23 09:12:43 -0400

    mount footer in preview

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,8 +1,76 @@
 import '@massds/mds-styles/index.css';
 import '@massds/mds-tokens/dist/index.css';
+import '@massds/mds-components/action-link.css';
+import '@massds/mds-components/footer.css';
+import '@massds/mds-components/icon-button.css';
+import { DocsContainer } from '@storybook/addon-docs/blocks';
+import { createElement, Fragment, useEffect, useState } from 'react';
+import storybookPackage from '../package.json';
 import './docs-markdown.css';
 import './preview.css';
 import { massdsDocsTheme } from './theme';
+
+const footerHtmlUrl = new URL(
+  `${import.meta.env.BASE_URL}components/footer/footer.storybook.html`,
+  window.location.origin
+).href;
+const stateAssetsVersion = storybookPackage.dependencies['@massds/mds-assets'];
+const footerSealSrc = `https://unpkg.com/@massds/mds-assets@${stateAssetsVersion}/dist/state-seal/state-seal-black.png`;
+let footerMarkupPromise;
+
+function loadFooterMarkup() {
+  if (!footerMarkupPromise) {
+    footerMarkupPromise = fetch(footerHtmlUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Unable to load the Storybook footer: ${response.status}`);
+        }
+
+        return response.text();
+      })
+      .then((markup) => {
+        const template = document.createElement('template');
+
+        template.innerHTML = markup;
+        template.content.querySelector('.mds-footer__seal')?.setAttribute('src', footerSealSrc);
+        return template.innerHTML;
+      });
+  }
+
+  return footerMarkupPromise;
+}
+
+function StorybookDocsContainer({ children, ...props }) {
+  const [footerMarkup, setFooterMarkup] = useState('');
+
+  useEffect(() => {
+    let isMounted = true;
+
+    loadFooterMarkup()
+      .then((markup) => {
+        if (isMounted) {
+          setFooterMarkup(markup);
+        }
+      })
+      .catch((error) => console.error(error));
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return createElement(
+    Fragment,
+    null,
+    createElement(DocsContainer, props, children),
+    footerMarkup
+      ? createElement('div', {
+        id: 'storybook-docs-footer',
+        dangerouslySetInnerHTML: { __html: footerMarkup }
+      })
+      : null
+  );
+}
 
 const preview = {
   parameters: {
@@ -29,6 +97,7 @@ const preview = {
     },
     docs: {
       theme: massdsDocsTheme,
+      container: StorybookDocsContainer,
       toc: {
         headingSelector: 'h2',
         title: null
